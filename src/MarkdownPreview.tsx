a126eaa2c5e7ef1a50e1322a7313c7baf106735d
Joe Attardi <jattardi@gmail.com>
2025-04-16 21:55:29 -0400

    adjust preview background

--- src/MarkdownPreview.tsx
+++ src/MarkdownPreview.tsx
@@ -9,7 +9,7 @@ export default function MarkdownPreview({ markdown }: MarkdownPreviewProps) {
         <div className="flex flex-col bg-slate-200 overflow-hidden">
             <header className="px-2 py-1 border border-slate-300 border-b-0 border-t-0 uppercase text-xs tracking-wide">Preview</header>
             <div
-                className="bg-slate-50 flex-grow border border-slate-300 p-4 prose max-w-none overflow-auto"
+                className="bg-slate-100 flex-grow border border-slate-300 p-4 prose max-w-none overflow-auto"
             >
                 <Markdown>{markdown}</Markdown>
             </div>  
