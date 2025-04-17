49d16abc480bacb65d2f7beffbc854ab197a44b3
Joe Attardi <jattardi@gmail.com>
2025-04-16 21:11:48 -0400

    Header style changes

--- src/MarkdownEditor.tsx
+++ src/MarkdownEditor.tsx
@@ -6,7 +6,7 @@ type MarkdownEditorProps = {
 export default function MarkdownEditor({ markdown, onChange }: MarkdownEditorProps) {
     return (
         <div className="flex flex-col bg-slate-200">
-            <header className="px-2 py-1 border border-slate-300 border-b-0 border-t-0">Editor</header>
+            <header className="px-2 py-1 border border-slate-300 border-b-0 border-t-0 uppercase text-xs tracking-wide">Editor</header>
             <textarea
                 className="bg-slate-50 flex-grow border border-slate-300 font-mono p-4 outline-none"
                 value={markdown}
--- src/MarkdownPreview.tsx
+++ src/MarkdownPreview.tsx
@@ -7,7 +7,7 @@ type MarkdownPreviewProps = {
 export default function MarkdownPreview({ markdown }: MarkdownPreviewProps) {
     return (
         <div className="flex flex-col bg-slate-200">
-            <header className="px-2 py-1 border border-slate-300 border-b-0 border-t-0">Preview</header>
+            <header className="px-2 py-1 border border-slate-300 border-b-0 border-t-0 uppercase text-xs tracking-wide">Preview</header>
             <div
                 className="bg-slate-50 flex-grow border border-slate-300 p-4 prose max-w-none"
             >
