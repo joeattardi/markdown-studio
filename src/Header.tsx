e820106f1f515e4472fcc68f4f872d5c0c77d04f
Joe Attardi <jattardi@gmail.com>
2025-04-18 16:34:34 -0400

    Shrink header font size a little

--- src/Header.tsx
+++ src/Header.tsx
@@ -5,7 +5,7 @@ export default function Header({ view, setView }) {
         <header className="bg-white text-slate-600 p-2 flex items-center justify-between">
             <div className="flex items-center gap-2">
                 <img src="/logo.png" alt="Logo" className="w-8" />
-                <h1 className="text-xl">Markdown Studio</h1>
+                <h1 className="text-lg">Markdown Studio</h1>
             </div>
             <ViewSelector view={view} setView={setView} />
         </header>
