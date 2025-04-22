bcc06d2f9f53e0d1dc954665a7c47cbe1e7a65fa
Joe Attardi <jattardi@gmail.com>
2025-04-21 20:36:55 -0400

    Tweak logo

--- src/components/app/Header.tsx
+++ src/components/app/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
         <header className="bg-white text-slate-600 p-2 flex items-center justify-between">
             <div className="flex items-center gap-2">
                 <img src="/logo.png" alt="Logo" className="w-8" />
-                <h1 className="text-lg">Typemark</h1>
+                <h1 className="text-xl font-semibold">Typemark</h1>
             </div>
             <ViewSelector />
         </header>
